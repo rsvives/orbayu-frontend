a9907d323ab693f847873bf40d175e05aca17769
Rodri <r.serranvives@gmail.com>
2025-10-07 19:15:31 +0200

    feat: improved weather service url and response handling

--- src/services/weatherService.ts
+++ src/services/weatherService.ts
@@ -1,14 +1,16 @@
 import axios from "axios"
 
+const API_URL = import.meta.env.PROD ? import.meta.env.VITE_ENV_API_URL : "http://192.168.1.149:8000"
+
 export const weatherService = {
     getWeather: async ({ lat, lon, radius = 30000, start_date, end_date }: { lat: number | undefined | null, lon: number | undefined | null, radius: number, start_date: string, end_date: string }) => {
         console.log('getting weather data')
         console.log(new Date(start_date).toLocaleString('default'))
-        const response = await axios.post('http://localhost:8000/weather_check',
+        const response = await axios.post(API_URL + '/weather_check',
             {
                 lat,
                 lon,
-                radius,
+                radius: radius * 1000,
                 start_date: new Date(start_date).toLocaleString('default'),
                 end_date: new Date(end_date).toLocaleString('default')
             }, {
@@ -18,7 +20,11 @@ export const weatherService = {
             }
         }
         )
-        console.log(response)
-        return response
+        // console.log(response.data)
+        // useWeatherDataStore.getState().setWeatherData(response.data)
+        if (response.status !== 200) {
+            throw new Error(response.data)
+        }
+        return response.data
     }
 }
