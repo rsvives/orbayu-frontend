004b6483ccf165aa152b383f0d9f5f771aa42443
Rodri <r.serranvives@gmail.com>
2025-10-07 19:13:28 +0200

    feat: added som date utils

--- src/lib/dates.ts
+++ src/lib/dates.ts
@@ -1,10 +1,30 @@
-export function formatDate(date: Date | undefined) {
+
+
+export function formatDate(date: Date | undefined, locale?: string, format?: Intl.DateTimeFormatOptions) {
     if (!date) {
         return ""
     }
-    return date.toLocaleDateString("en-US", {
+    return date.toLocaleDateString(locale || "en-US", format || {
         day: "2-digit",
-        month: "numeric",
+        month: "short",
         year: "numeric",
     })
+}
+
+export function addDays(date: Date, type: 'day' | 'week' | 'range' | 'month'): Date {
+
+    let adding = 0
+    switch (type) {
+        case 'day':
+            adding = 1
+            break;
+        case 'week':
+            adding = 7
+            break;
+        case 'month':
+            adding = 30
+            break;
+    }
+    const newDate = new Date((date.getTime() + adding * 24 * 60 * 60 * 1000) - 1000)
+    return newDate
 }
