4005b2ff5fc3bab4f9213ea5c15314d72a4214a0
Rodri <r.serranvives@gmail.com>
2025-10-13 11:26:34 +0200

    refactor: improved current location service function

--- src/services/location/getCurrentLocation.ts
+++ src/services/location/getCurrentLocation.ts
@@ -1,28 +1,16 @@
 export const getCurrentLocation = async (): Promise<{ lat: number, lng: number, accuracy: number }> => {
     return new Promise((resolve, reject) => {
-        if ('geolocation' in navigator) {
-            navigator.geolocation.getCurrentPosition(
-                (position) => {
-                    console.log({ position })
-                    resolve({
-                        lat: position.coords.latitude,
-                        lng: position.coords.longitude,
-                        accuracy: position.coords.accuracy,
-                    })
-                },
-                (err) => {
-                    console.error(err)
-                    reject(new Error(err.message))
-                },
-                {
-                    enableHighAccuracy: true,
-                    timeout: 5000,
-                    maximumAge: 0
-                }
-            )
-        } else {
-            console.error('Geolocation not available')
-            reject(new Error('Geolocation not available'))
+        if (!navigator.geolocation) {
+            reject(new Error('Geolocalización no disponible'));
         }
+
+        navigator.geolocation.getCurrentPosition(
+            (position) => resolve({
+                lat: position.coords.latitude,
+                lng: position.coords.longitude,
+                accuracy: position.coords.accuracy || 0
+            }),
+            (error) => reject(error)
+        );
     })
 }
