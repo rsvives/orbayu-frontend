ec4bbad6dd3ed400e2d96a7b49ce248ba712511a
Rodri <r.serranvives@gmail.com>
2025-10-17 10:33:42 +0200

    fix: vercel analytics?

--- src/main.tsx
+++ src/main.tsx
@@ -4,18 +4,18 @@ import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Header } from './components/Header.tsx'
 import { Toaster } from 'sonner'
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { Analytics } from "@vercel/analytics/next"
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <>
     <Analytics />
     <QueryClientProvider client={queryClient}>
       <Header />
       <App />
       <Toaster position='bottom-center' />
     </QueryClientProvider>
-  </StrictMode>
+  </>
 )
