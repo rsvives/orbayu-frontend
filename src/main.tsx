82027e89203cd4d5a34a5ceea18c88b35666b021
Rodri <r.serranvives@gmail.com>
2025-10-17 10:24:58 +0200

    feat: vercel analytics added

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,13 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Header } from './components/Header.tsx'
 import { Toaster } from 'sonner'
 import { StrictMode } from 'react'
+import { Analytics } from "@vercel/analytics/next"
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Analytics />
     <QueryClientProvider client={queryClient}>
       <Header />
       <App />
